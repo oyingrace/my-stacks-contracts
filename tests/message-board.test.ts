20e9ce24d4f9c040178e2b1329bb913d1fbe5cdf
oyingrace <oyingrace307@gmail.com>
2025-12-12 13:22:00 +0100

    Add tests for message-board contract: validate message addition and fund withdrawal functionality, ensuring correct event emissions and state updates.

--- tests/message-board.test.ts
+++ tests/message-board.test.ts
@@ -1,21 +1,60 @@
-
+import { Cl, ClarityType } from "@stacks/transactions";
 import { describe, expect, it } from "vitest";
 
 const accounts = simnet.getAccounts();
+const deployer = accounts.get("deployer")!;
 const address1 = accounts.get("wallet_1")!;
 
-/*
-  The test below is an example. To learn more, read the testing documentation here:
-  https://docs.hiro.so/stacks/clarinet-js-sdk
-*/
-
 describe("example tests", () => {
-  it("ensures simnet is well initialised", () => {
-    expect(simnet.blockHeight).toBeDefined();
+  let content = "Hello Stacks Devs!"
+
+  it("allows user to add a new message", () => {
+    let currentBurnBlockHeight = simnet.burnBlockHeight;
+
+    let confirmation = simnet.callPublicFn(
+      "message-board",
+      "add-message",
+      [Cl.stringUtf8(content)],
+      address1
+    )
+
+    const messageCount = simnet.getDataVar("message-board", "message-count");
+    
+    expect(confirmation.result).toHaveClarityType(ClarityType.ResponseOk);
+    expect(confirmation.result).toBeOk(messageCount);    
+    expect(confirmation.events[1].data.value).toBeTuple({
+      author: Cl.standardPrincipal(address1),
+      event: Cl.stringAscii("[Stacks Dev Quickstart] New Message"),
+      id: messageCount,
+      message: Cl.stringUtf8(content),
+      time: Cl.uint(currentBurnBlockHeight),
+    });
   });
 
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
+  it("allows contract owner to withdraw funds", () => {
+    simnet.callPublicFn(
+      "message-board",
+      "add-message",
+      [Cl.stringUtf8(content)],
+      address1
+    )
+    
+    simnet.mineEmptyBurnBlocks(2);
+
+    let confirmation = simnet.callPublicFn(
+      "message-board",
+      "withdraw-funds",
+      [],
+      deployer
+    )
+    
+    expect(confirmation.result).toBeOk(Cl.bool(true));
+    expect(confirmation.events[0].event).toBe("ft_transfer_event")
+    expect(confirmation.events[0].data).toMatchObject({
+      amount: '1',
+      asset_identifier: 'SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4.sbtc-token::sbtc-token',
+      recipient: deployer,
+      sender: `${deployer}${".message-board"}`,
+    })
+  })
 });
