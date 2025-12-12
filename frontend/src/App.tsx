399303f6e3dc2bf0a0f21a13d7943e2b40d4ab20
oyingrace <oyingrace307@gmail.com>
2025-12-12 14:24:03 +0100

    Enhance App component to include wallet connection and disconnection functionality, allowing users to connect their wallets and retrieve BNS names. Implement state management for wallet information and BNS name display.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
-import { request } from '@stacks/connect'
-import type { TransactionResult } from '@stacks/connect/dist/types/methods'
-import { Cl, Pc } from '@stacks/transactions'
+import { connect, disconnect, request } from '@stacks/connect'
+import type { GetAddressesResult, TransactionResult } from '@stacks/connect/dist/types/methods'
 import { useState } from 'react'
+import { Cl, Pc } from '@stacks/transactions'
 
 export default function App() {
-  // ...
+  let [isConnected, setIsConnected] = useState<boolean>(false)
+  let [walletInfo, setWalletInfo] = useState<any>(null)
+  let [bns, setBns] = useState<string>('')
+
+  async function connectWallet() {
+    let connectionResponse: GetAddressesResult = await connect()
+    let bnsName = await getBns(connectionResponse.addresses[2].address)
+
+    setIsConnected(true)
+    setWalletInfo(connectionResponse)
+    setBns(bnsName)
+  }
+
+  async function disconnectWallet() {
+    disconnect();
+  }
+
   let [content, setContent] = useState<string>('')
 
   async function addMessage() {
@@ -24,11 +40,25 @@ export default function App() {
   
     setContent('')
   }
+  
+  async function getBns(stxAddress: string) {
+    let response = await fetch(`https://api.bnsv2.com/testnet/names/address/${stxAddress}/valid`)
+    let data = await response.json()
 
+    return data.names[0].full_name
+  }
+  
   return (
     <>
-      // ...
-      <span className='input-container'>
+      <h3>Stacks Dev Quickstart Message Board</h3>
+      {isConnected ? (
+        <button onClick={disconnectWallet}>{
+          bns ? bns : walletInfo.addresses[2].address
+        }</button>
+      ) : (
+        <button onClick={connectWallet}>connect wallet</button>
+      )}
+        <span className='input-container'>
         <button onClick={addMessage}>add-message</button>
         <input type="text" onChange={e => setContent(e.target.value)}/>
       </span>
